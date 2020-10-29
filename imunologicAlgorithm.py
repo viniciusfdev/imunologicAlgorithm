96ff18afcadb4725ae155f38a3cec76b3660b348
Vinicius F <vinilima07@gmail.com>
2020-10-28 23:10:14 -0300

    add code comment

--- imunologicAlgorithm.py
+++ imunologicAlgorithm.py
@@ -1,3 +1,17 @@
+# python imunologic algortihm  
+
+### Algoritmos Imunológicos  
+##### Autor: Vinícius França Lima Vilaça  
+
+# a) Max_it=50  
+# b) n1=N=50  
+# c) n2=0  
+# d) beta=0,1  
+# e) Nc=beta*N – Define o número de clones a ser gerado para cada anticorpo  
+# f) alpha - parâmetro da equação de mutação = 5  
+
+
+
 import matplotlib.pyplot as plt
 import numpy as np
 import random
