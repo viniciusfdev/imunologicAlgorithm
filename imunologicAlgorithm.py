198e45f74e07dc632134d8dca61e98b03b44434a
Vinicius França <vinilima07@gmail.com>
2020-10-30 10:36:34 -0300

    Update imunologicAlgorithm.py

--- imunologicAlgorithm.py
+++ imunologicAlgorithm.py
@@ -1,14 +1,14 @@
 # python imunologic algortihm  
-
 ### Algoritmos Imunológicos  
 ##### Autor: Vinícius França Lima Vilaça  
+##### clound link: https://colab.research.google.com/drive/1gM0eIguibcskQJ_HxfMFNgYcQzePGBPh?usp=sharing
 
-# a) Max_it=50  
-# b) n1=N=50  
-# c) n2=0  
-# d) beta=0,1  
-# e) Nc=beta*N – Define o número de clones a ser gerado para cada anticorpo  
-# f) alpha - parâmetro da equação de mutação = 5  
+### a) Max_it=50  
+### b) n1=N=50  
+### c) n2=0  
+### d) beta=0,1  
+### e) Nc=beta*N – Define o número de clones a ser gerado para cada anticorpo  
+### f) alpha - parâmetro da equação de mutação = 5  
 
 
 
